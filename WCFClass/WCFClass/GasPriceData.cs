82b8b1ca3317cd870b065ae040e23924aa291d63
SuYonghuan <1147948702@qq.com>
2016-05-27 18:48:36 +0800

    first
    第一个版本

---
+++ WCFClass/WCFClass/GasPriceData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+
+namespace WCFClass
+{
+    [DataContract]
+    public class GasPriceData
+    {
+        [DataMember]
+        public string GasStation { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+    }
+}
---
+++ WCFClass/WebApplication1/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using WCFClass;
+namespace WebApplication1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            System.ServiceModel.Activation.WebServiceHostFactory WSHF =
+      new System.ServiceModel.Activation.WebServiceHostFactory();
+
+            System.ServiceModel.Activation.ServiceRoute ss =
+              new System.ServiceModel.Activation.ServiceRoute(
+              "wcfservice", WSHF, typeof(WCFClass.GasPriceService));
+            System.Web.Routing.RouteTable.Routes.Add(ss);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
