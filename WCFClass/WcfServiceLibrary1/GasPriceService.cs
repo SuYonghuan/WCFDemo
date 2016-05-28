ffa5bd29b97115387cb8090c8e6f9efe7f0af257
suyonghuan <1147948702@qq.com>
2016-05-28 16:00:52 +0800

    gengxin

---
+++ WCFClass/WcfServiceLibrary1/GasPriceService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WCFClass
+{
+    [AspNetCompatibilityRequirements(
+        RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class GasPriceService : IGasPriceService
+    {
+        #region Members
+
+        public GasPriceData GetPriceByZipCode(string zipCode)
+        {
+            switch (zipCode)
+            {
+                case "00000":
+                    return new GasPriceData { GasStation = "None", Price = 0.00m };
+                case "12345":
+                    return new GasPriceData { GasStation = "HP", Price = 420m };
+                case "11111":
+                    return new GasPriceData { GasStation = "Indian Oil", Price = 531m };
+                default:
+                    return new GasPriceData { GasStation = "DefaultGasPrice", Price = 400m };
+            }
+        }
+
+        public GasPriceData GetPriceByCity(string city)
+        {
+            switch (city.ToLower())
+            {
+                case "hyderabad":
+                    return new GasPriceData { GasStation = "HP", Price = 459m };
+                case "delhi":
+                    return new GasPriceData { GasStation = "Indian Oil", Price = 449m };
+                case "chenni":
+                    return new GasPriceData { GasStation = "BG", Price = 499m };
+                default:
+                    return new GasPriceData { GasStation = "DefaultGasPrice", Price = 409m };
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ WCFClass/WcfServiceLibrary1/IGasPriceService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WCFClass
+{
+    [ServiceContract]
+    public interface IGasPriceService
+    {
+        [OperationContract]
+        [WebGet
+            (ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/GetGasPrice/For/ZipCode/{zipCode}"
+            )]
+        GasPriceData GetPriceByZipCode(string zipCode);
+
+        [OperationContract]
+        [WebGet
+            (RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/GetGasPrice/For/City/{city}"
+            )]
+        GasPriceData GetPriceByCity(string city);
+    }
+}
